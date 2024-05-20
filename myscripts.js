d4fe0f2b52f12b348f13cff3b6099c4744319bbd
lukas paul <lukas@172.31.172.200>
2024-05-20 21:05:24 +0200

    implemented the game logic
    created the playRound(), playGame() functions

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,39 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("No winner this round");
+    } else if((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper"))  {
+        humanScore += 1;
+        console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+    } else {
+        computerScore += 1;
+        console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
+    }
+}
+
+function playGame() {
+    humanScore = 0;
+    computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    if (humanScore === computerScore) {
+        console.log(humanScore + " to " + computerScore + " you are equally strong!");
+    } else if(humanScore > computerScore) {
+        console.log(humanScore + " to " + computerScore + " you have beaten the machine!");
+    } else {
+        console.log(computerScore + " to " + humanScore + "You lost, the machines are taking over!");
+    }
+}
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
 function getComputerChoice() {
-    var randomNumber = getRandomInt(3);
+    let randomNumber = getRandomInt(3);
     switch (randomNumber) {
         case 0:
             return "rock";
@@ -12,4 +42,12 @@ function getComputerChoice() {
         case 2:
             return "scissors";
     }
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Write your choice below").toLowerCase();
+    while(!(humanChoice === "rock" || humanChoice === "scissors" || humanChoice === "paper")) {
+        humanChoice = prompt("Please enter a valid option below. Choose from (rock, paper, scissors)").toLowerCase();
+    }
+    return humanChoice;
 }
